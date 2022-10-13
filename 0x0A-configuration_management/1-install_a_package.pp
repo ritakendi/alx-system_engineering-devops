d64a63e354039b6568da2b2f355bf14fcea10d44
ritakendi <ritabetty77@gmail.com>
2022-10-13 11:30:53 -0700

    pip3 as provider

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
-# installs the package puppet-lint
-package { 'puppet-lint':
-  ensure   => '2.1.1',
-  provider => 'gem',
+# Install flask from pip3
+	package { 'Flask':
+		ensure   => '2.1.0',
+		provider => 'pip3',
 }
